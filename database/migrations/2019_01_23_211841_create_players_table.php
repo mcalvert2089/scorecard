e46f90f03714877bdfd2d81adde07adf3d12c7fa
Matt Calvert <mbcalver@gmail.com>
2019-01-23 16:37:27 -0500

    added models, seeders, factories for players

---
+++ database/migrations/2019_01_23_211841_create_players_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePlayersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('players', function (Blueprint $table) {
+            $table->primary('id');
+            $table->uuid('id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->uuid('team_id');
+            $table->char('bats', 1)->nullable();
+            $table->char('throws', 1)->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('players');
+    }
+}
