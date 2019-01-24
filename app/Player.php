1cff53f588f38612a07ce2a033ef5ab7486014a4
Matt Calvert <mbcalver@gmail.com>
2019-01-24 11:07:44 -0500

    removed unneeded functions

--- app/Player.php
+++ app/Player.php
@@ -14,29 +14,4 @@ class Player extends Model
     public $incrementing = false;
 
     protected $fillable = [ 'first_name', 'last_name', 'team_id', 'user_id', 'bats', 'throws' ];
-
-    public function createPlayer($request, $user_id) {
-		$data = [];
-		$data['first_name'] = $request->first_name;
-		$data['last_name'] = $request->last_name;
-		$data['team_id'] = $request->team_id;
-    	$data['user_id'] = $user_id;
-    	$data['bats'] = $request->bats;
-    	$data['throws'] = $request->throws;
-    	
-        Player::create($data);
-    }
-
-    public function updatePlayer($request, $id) {
-    	$player = Player::find($id);
-
-        return tap($player)->update([
-		        	'first_name' => $request->first_name,
-			    	'last_name' => $request->last_name,
-			    	'team_id' => $request->team_id,
-			    	'user_id' => $request->user_id,
-			    	'bats' => $request->bats,
-			    	'throws' => $request->throws
-		        ]);
-    }
 }
--- app/Team.php
+++ app/Team.php
@@ -10,9 +10,4 @@ class Team extends Model
 	use Uuids, SoftDeletes;
     public $incrementing = false;
     protected $fillable = [ 'name', 'manager', 'city', 'state', 'user_id' ];
-
-    public function getAll($userId) {
-    	$teams = Team::where('user_id', $userId)->get();
-    	return $teams;
-    }
 }
